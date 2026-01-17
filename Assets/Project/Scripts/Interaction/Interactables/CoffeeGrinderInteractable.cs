d14e3d4cb6af06cdcb6153a3cf5d8377094f179d
Daniel Pokladek <danielpokladek@proton.me>
2026-01-17 13:51:01 +0000

    completes coffee grinder interactions (not functional yet)

--- Assets/Project/Scripts/Interaction/Interactables/CoffeeGrinderInteractable.cs
+++ Assets/Project/Scripts/Interaction/Interactables/CoffeeGrinderInteractable.cs
@@ -1,13 +1,37 @@
+using PrimeTween;
 using UnityEngine;
 
 public class CoffeeGrinderInteractable : Interactable
 {
+    [SerializeField]
+    private GameObject _beanCanvas;
+
     [SerializeField]
     private float _grindDuration = 2f;
 
     private float _grindTimer = 0;
 
     private bool _isGrinding = false;
+    private bool _hasBeans = false;
+
+    private Sequence _beansSequence;
+
+    private void Start()
+    {
+        _beanCanvas.SetActive(false);
+
+        _beansSequence = Sequence
+            .Create(-1, Sequence.SequenceCycleMode.Yoyo, Ease.InOutSine)
+            .Group(
+                Tween.PositionY(
+                    _beanCanvas.transform,
+                    _beanCanvas.transform.position.y + 0.5f,
+                    1.5f
+                )
+            );
+
+        _beansSequence.isPaused = true;
+    }
 
     private void Update()
     {
@@ -19,9 +43,15 @@ public class CoffeeGrinderInteractable : Interactable
         if (_grindTimer >= _grindDuration)
         {
             _isGrinding = false;
+            _hasBeans = true;
 
             _interactionPrompt.UpdateTimerFill(0);
             _interactionPrompt.ShowPrompt();
+
+            _beanCanvas.SetActive(true);
+
+            _beansSequence.progressTotal = 0;
+            _beansSequence.isPaused = false;
 
             IsBusy = false;
 
@@ -34,6 +64,14 @@ public class CoffeeGrinderInteractable : Interactable
 
     public override void Interact()
     {
+        if (_hasBeans)
+        {
+            _hasBeans = false;
+            _beanCanvas.SetActive(false);
+
+            return;
+        }
+
         base.Interact();
 
         IsBusy = true;
